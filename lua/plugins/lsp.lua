a0ff492262d8a8d18d8f8e0a7015b1ee3ac93912
Josef Weisböck <eduweijos001@fh-kaernten.at>
2024-10-21 20:47:19 +0200

    require lua ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -82,6 +82,7 @@ return {
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'gopls',
+        'lua-language-server'
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
