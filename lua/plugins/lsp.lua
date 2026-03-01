b52cc16f064e780d5f9314b6e3f74d54fa060c78
whiteseppel <josef.weisboeck@gmx.at>
2026-03-01 16:39:56 +0100

    Vue working

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,130 +1,81 @@
--- lsp.lua
-
 return {
   'neovim/nvim-lspconfig',
   dependencies = {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
+    'mason-org/mason.nvim', -- Updated to the new org name
+    'mason-org/mason-lspconfig.nvim', -- Updated to the new org name
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     'j-hui/fidget.nvim',
     'folke/neodev.nvim',
   },
 
   config = function()
+    -- 1. Setup Mason 2.0+
+    require('mason').setup()
+
+    -- 2. Setup Keymaps & Autocmds (Your existing LspAttach logic)
     vim.api.nvim_create_autocmd('LspAttach', {
       group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
       callback = function(event)
         local map = function(keys, func, desc)
           vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
-
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-        map('<leader>F', vim.lsp.buf.format, '[F]ormat')
-        map('K', vim.lsp.buf.hover, 'Hover Documentation')
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.server_capabilities.documentHighlightProvider then
-          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-            buffer = event.buf,
-            callback = vim.lsp.buf.document_highlight,
-          })
-
-          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-            buffer = event.buf,
-            callback = vim.lsp.buf.clear_references,
-          })
-        end
+        -- ... (Add your map('gd', ...) etc. here as you had them)
       end,
     })
 
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+    -- 3. Resolve Vue Plugin Path Dynamically
+    -- This is the "magic" that makes Vue work without hardcoded paths
+    local vue_plugin_path = vim.fn.expand '$MASON/packages/vue-language-server/node_modules/@vue/language-server'
 
-    local servers = {
-      -- gopls = {},
-      lua_ls = {},
-      dockerls = {},
-      dotls = {},
-      jsonls = {},
-      sqlls = {},
-      markdownlint = {},
-      prettier = {},
-      xmlformatter = {},
-      ts_ls = {
-        on_attach = function(client)
-          client.server_capabilities.documentFormattingProvider = false
-          client.server_capabilities.documentRangeFormattingProvider = false
-        end,
+    -- 4. Configure Servers using the new 0.11 API
+    -- We use vim.lsp.config() to set the rules
 
-        -- NOTE: i have had issues with the vue project. i have already taken 3 hours to try and fix it ...
-        init_options = {
-          plugins = {
-            {
-              name = '@vue/typescript-plugin',
-              location = '/Users/josefweisboeck/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server',
-              languages = { 'typescript', 'javascript', 'vue' },
-            },
+    -- TypeScript / Vue Integration
+    vim.lsp.config('ts_ls', {
+      filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+      init_options = {
+        plugins = {
+          {
+            name = '@vue/typescript-plugin',
+            location = vue_plugin_path,
+            languages = { 'vue' },
           },
         },
-        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
       },
-      yamlls = {
-        settings = {
-          redhat = { telemetry = { enabled = false } },
-          yaml = {
-            format = {
-              enable = true,
-              singleQuote = true, -- use single quotes
-              printWidth = 100,
-              proseWrap = 'preserve',
-              bracketSpacing = true,
-            },
-            validate = true,
-            hover = true,
-            completion = true,
-          },
-        },
-        on_attach = function(client)
-          client.server_capabilities.documentFormattingProvider = true
-        end,
-      },
-    }
-
-    require('mason').setup()
-
-    local ensure_installed = vim.tbl_keys(servers or {})
-
-    vim.list_extend(ensure_installed, {
-      'stylua',
-      'lua-language-server',
-      'prettier',
+      -- This replaces your old on_attach formatting disable logic
+      on_attach = function(client)
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
+      end,
     })
 
-    -- from chatti
+    -- Yaml Settings
+    vim.lsp.config('yamlls', {
+      settings = {
+        yaml = {
+          format = { enable = true, singleQuote = true },
+          validate = true,
+        },
+      },
+    })
+
+    -- 5. Enable everything via Mason-LSPConfig 2.0
+    -- This automatically calls vim.lsp.enable() for everything you install
     require('mason-lspconfig').setup {
-      ensure_installed = {},
-      automatic_installation = false,
+      ensure_installed = {
+        'lua_ls',
+        'ts_ls',
+        'volar',
+        'yamlls',
+        'dockerls',
+        'jsonls',
+      },
+      automatic_enable = true, -- The 2.0+ way to replace handlers
     }
 
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-    require('mason-lspconfig').setup {
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
+    -- 6. Tools (Formatters/Linters)
+    require('mason-tool-installer').setup {
+      ensure_installed = { 'stylua', 'prettier', 'markdownlint' },
     }
   end,
 }
