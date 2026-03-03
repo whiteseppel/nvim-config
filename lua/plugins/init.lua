fdd58b709482488e13d0c94eed246730d1fadd88
Josef Weisböck <josef@autosense.ch>
2026-03-03 16:08:51 +0100

    Make it run again

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,6 +8,48 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
 
+  { -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+    branch = 'main',
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
+    config = function()
+      local parser_dir = vim.fn.stdpath 'data' .. '/site'
+      vim.opt.runtimepath:append(parser_dir)
+
+      local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+      require('nvim-treesitter').install(parsers)
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(args)
+          local buf, filetype = args.buf, args.match
+
+          local language = vim.treesitter.language.get_lang(filetype)
+          if not language then
+            return
+          end
+
+          -- check if parser exists and load it
+          if not vim.treesitter.language.add(language) then
+            return
+          end
+          -- enables syntax highlighting and other treesitter features
+          vim.treesitter.start(buf, language)
+
+          -- enables treesitter based folds
+          -- for more info on folds see `:help folds`
+          -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          -- vim.wo.foldmethod = 'expr'
+
+          -- enables treesitter based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
+  },
+
+  require 'plugins.telescope',
+
   {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
@@ -39,20 +81,6 @@ require('lazy').setup({
       },
     },
   },
-
-  require 'plugins.tree',
-  require 'plugins.flutter-tools',
-  require 'plugins.telescope',
-  require 'plugins.lsp',
-  require 'plugins.debug',
-  require 'plugins.which-key',
-  require 'plugins.lint',
-  require 'plugins.lazygit',
-  require 'plugins.codecompanion',
-  require 'plugins.render-markdown',
-
-  -- TODO: still needs some work - debugger does not connect
-  -- require('kickstart.plugins.debug_node'),
 
   {
     'nvimtools/none-ls.nvim',
@@ -251,27 +279,47 @@ require('lazy').setup({
       end
     end,
   },
-  { -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
-    end,
-  },
+  -- { -- Fuzzy Finder (files, lsp, etc)
+  --   'nvim-telescope/telescope.nvim',
+  --   opts = {
+  --     defaults = {
+  --       preview = {
+  --         treesitter = false,
+  --       },
+  --     },
+  --   },
+  --   enabled = true,
+  --   event = 'VimEnter',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     { -- If encountering errors, see telescope-fzf-native README for installation instructions
+  --       'nvim-telescope/telescope-fzf-native.nvim',
+  --
+  --       build = 'make',
+  --
+  --       cond = function()
+  --         return vim.fn.executable 'make' == 1
+  --       end,
+  --     },
+  --     { 'nvim-telescope/telescope-ui-select.nvim' },
+  --
+  --     -- Useful for getting pretty icons, but requires a Nerd Font.
+  --     { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+  --   },
+  -- },
+
+  require 'plugins.tree',
+  require 'plugins.flutter-tools',
+  require 'plugins.lsp',
+  require 'plugins.debug',
+  require 'plugins.which-key',
+  require 'plugins.lint',
+  require 'plugins.lazygit',
+  require 'plugins.codecompanion',
+  require 'plugins.render-markdown',
+
+  -- TODO: still needs some work - debugger does not connect
+  -- require('kickstart.plugins.debug_node'),
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
